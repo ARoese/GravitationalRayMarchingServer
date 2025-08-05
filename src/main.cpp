d29caf806e6969511f6233b68d1ef2f5fff72f51
Atom Roese <40679658+ARoese@users.noreply.github.com>
2025-08-05 07:47:44 -0400

    switch to vcpkg and allow selecting of listen port

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,26 @@
 #include "libgrmproto/Material.pb.h"
 #include "boost/asio.hpp"
 #include "libgrmproto/Render.pb.h"
+#include "boost/program_options.hpp"
 
 using boost::asio::ip::tcp;
+namespace po = boost::program_options;
 
-int main() {
-    auto material = grm::protobuf::Material();
-    auto const mc = material.mutable_color();
-    mc->set_x(1);
-    mc->set_y(2);
-    mc->set_z(3);
-    material.SerializeToOstream(&std::cout);
-    std::cout << std::endl;
-    std::cout << material.DebugString() << std::endl;
+int main(int argc, char *argv[]) {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("port", po::value<unsigned short>()->default_value(0), "port to listen on. Use 0 (default) to have one picked for you.");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    auto listen_port = vm["port"].as<unsigned short>();
 
     boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 9000));
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), listen_port));
+    std::cout << "listening on port " << acceptor.local_endpoint().port() << std::endl;
     while (true) {
         auto connectionStream = tcp::iostream();
         acceptor.accept(connectionStream.socket());
